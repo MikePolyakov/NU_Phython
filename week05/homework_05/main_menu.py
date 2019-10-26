c1e9faac49950b1071b85c91b1177a66a047737c
MikePolyakov <id2k1149@gmail.com>
2019-10-26 06:32:19 +0300

    homework 5

--- week05/homework_05/main_menu.py
+++ week05/homework_05/main_menu.py
@@ -1,8 +1,6 @@
 # Main menu
 import os
 import shutil
-from os import path
-from os import walk
 import os_name
 import quiz
 from bank_account import personal_account
@@ -25,8 +23,10 @@ while True:
     choice = input('Выберите пункт меню ')
     if choice == '1':
         folder_name = input('Дайте имя папки - ')
-        if not path.exists(folder_name):
+        if not os.path.exists(folder_name):
             os.mkdir(folder_name)
+        else:
+            print(f'Папка {folder_name} уже есть')
     elif choice == '2':
         folder_name = input('Какую папку хотите удалить? ')
         os.rmdir(folder_name)
@@ -39,13 +39,13 @@ while True:
         print(*os.listdir(directory))
     elif choice == '5':
         only_folders = []
-        for (dirpath, dirnames, filenames) in walk(os.getcwd()):
+        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
             only_folders.extend(dirnames)
             break
         print(f'В текущей директории находятся папки:', *only_folders)
     elif choice == '6':
         only_files = []
-        for (dirpath, dirnames, filenames) in walk(os.getcwd()):
+        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
             only_files.extend(filenames)
             break
         print(f'В текущей директории находятся файлы:', *only_files)
